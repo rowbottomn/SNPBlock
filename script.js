ee7e41b4d203947fa4729412080f6efd04a83a4b
Nathan Rowbottom <nathan.rowbottom@snpsteam.com>
2019-11-26 13:19:36 +0000

    Finished form

---
+++ script.js
@@ -0,0 +1,3 @@
+
+  animation__mouseenter="property: components.material.material.color; type: color; to: blue; startEvents: mouseenter; dur: 500";
+  animation__mouseleave="property: components.material.material.color; type: color; to: red; startEvents: mouseleave; dur: 500";
